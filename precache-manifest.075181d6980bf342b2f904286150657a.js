46f79b0304b22ff17e3e8dfa55bd803394ed0d4a
eloygplaza <10582213+eloygplaza@users.noreply.github.com>
2022-10-16 13:52:37 +0200

    adding build

---
+++ precache-manifest.075181d6980bf342b2f904286150657a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d18ac376e7e4057a2613",
+    "url": "/jamming-app/static/css/main.88c3ca7b.chunk.css"
+  },
+  {
+    "revision": "d18ac376e7e4057a2613",
+    "url": "/jamming-app/static/js/main.13a9dec4.chunk.js"
+  },
+  {
+    "revision": "f5ead6152ef219ed321d",
+    "url": "/jamming-app/static/js/runtime~main.e8d19c3a.js"
+  },
+  {
+    "revision": "e9eea0971e8038d5e033",
+    "url": "/jamming-app/static/js/2.f6106045.chunk.js"
+  },
+  {
+    "revision": "79be80b375553eb66abb",
+    "url": "/jamming-app/static/js/3.c70b6b5b.chunk.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/jamming-app/static/media/background_photo_desktop.37be7357.jpg"
+  },
+  {
+    "revision": "9f32b50621ed8f0aafe46c59b1867829",
+    "url": "/jamming-app/index.html"
+  }
+];
